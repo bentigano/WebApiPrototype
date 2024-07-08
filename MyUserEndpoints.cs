647e01e3abc729d20f6eefde7fa9978f66b8d144
bentigano <bentigano@gmail.com>
2024-07-08 17:04:37 -0400

    Added group endpoint mapping.

--- MyUserEndpoints.cs
+++ MyUserEndpoints.cs
@@ -5,8 +5,22 @@
         // Extension method called from Program.cs
         public static void AddMyUserEndpoints(this IEndpointRouteBuilder app)
         {
+            // these get mapped to the root endpoint (/Register, /Update)
             app.MapGet("Register", () => { return "Registered"; });
             app.MapPut("Update", () => { return "Updated"; });
+
+            // these get mapped under the group name (/api/v17/things)
+            var things = app.MapGroup("/api/v17/things");
+            things.MapGet("", () => "return all things");
+            things.MapGet("/{id}", (int id) => $"return user with id {id}");
+            things.MapPost("", (Thing thing) => $"add user with id {thing.ID}");
+            things.MapPut("/{id}", (int id) => $"put user with id {id}");
+            things.MapDelete("/{id}", (int id) => $"delete user with id {id}");
         }
+    }
+
+    public class Thing
+    {
+        public int ID;
     }
 }
