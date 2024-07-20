9670bd349b3b63c20e27259ad80d8b96cd6ded44
bentigano <bentigano@gmail.com>
2024-07-20 07:52:28 -0400

    Removed additional details from the API key authorization response message that could be used by a bad actor.

--- MyApiKeyMiddleware.cs
+++ MyApiKeyMiddleware.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     /// Check to see if an X-Api-Key header was supplied and the valied is allowed based on a whitelist.
-    /// If not, return a 401 and a response of "Invalid or missing API Key supplied in header X-Api-Key.".
+    /// If not, return a 401 and a response of "Invalid or missing API Key".
     /// </summary>
     public class MyApiKeyMiddleware
     {
@@ -27,7 +27,7 @@
             if (!context.Request.Headers.TryGetValue(API_KEY_HEADER_NAME, out var receivedApiKey) || !_validApiKeys.Contains(receivedApiKey.ToString()))
             {
                 context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                await context.Response.WriteAsync($"Invalid or missing API Key supplied in header {API_KEY_HEADER_NAME}.");
+                await context.Response.WriteAsync($"Invalid or missing API Key.");
                 return;
             }
 
